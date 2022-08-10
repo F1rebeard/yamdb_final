7bf69e9373144c6535ab24f7c83982880a599fb3
Александр Саратов <Alx.Srtv@yandex.ru>
2022-08-10 13:33:25 +0300

    isort for flake8

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,18 +1,14 @@
 from django.core.mail import EmailMessage
-from rest_framework import viewsets, views, permissions, status
-from rest_framework.response import Response
+from rest_framework import permissions, status, views, viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .serializers import (
-    UserSerializer,
-    SignUpSerializer,
-    SignInSerializer,
-    UserInfoUpdateSerializer,
-)
 from .models import User
 from .permissions import IsAdmin
+from .serializers import (SignInSerializer, SignUpSerializer,
+                          UserInfoUpdateSerializer, UserSerializer)
 
 
 class APISignUp(views.APIView):
