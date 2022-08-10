7b655c3df31a04e19fab8c64035e6e6860d94a0d
Александр Саратов <Alx.Srtv@yandex.ru>
2022-08-10 13:18:00 +0300

    first commit

---
+++ api_yamdb/reviews/validators.py
@@ -0,0 +1,15 @@
+"""Кастомные валидаторы"""
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+
+def validate_year(value):
+    """
+    Валидатор года выпуска title
+    """
+    year = timezone.now().year
+    if value > year:
+        raise ValidationError(
+            'Нельзя добавлять фильмы из будущего! '
+            'Временная полиция не дремлет.'
+        )
